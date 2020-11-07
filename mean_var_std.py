7797ba4292b4af21f12550a443b0d4bc8be81c56
Tiago Silva <tiago.silva@email.com>
2020-11-07 00:39:49 -0300

    Initial Commit
    Initial Commit

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,21 @@
+"""
+Autor: Tiago Silva
+e-mail: tiago.silva@email.com
+freeCodeCamp --> Data Analysis Challenge #1
+This code calculates the mean, variance, standard deviation, min, max, sum for a given list with 9 number
+"""
 import numpy as np
-
 def calculate(list):
-
-
-
-
-    return calculations
+    if len(list)==9:
+        list = np.reshape(list, (3,3))
+        data_dict = {
+            'mean': [np.mean(list, axis=0).tolist(), np.mean(list, axis=1).tolist(), np.mean(list)],
+            'variance': [np.var(list,axis=0).tolist(), np.var(list,axis=1).tolist(), np.var(list)],
+            'standard deviation': [np.std(list,axis=0).tolist(), np.std(list,axis=1).tolist(), np.std(list)],
+            'max': [np.max(list,axis=0).tolist(),np.max(list,axis=1).tolist(), np.max(list)],
+            'min': [np.min(list,axis=0).tolist(), np.min(list, axis=1).tolist(), np.min(list)],
+            'sum': [np.sum(list,axis=0).tolist(), np.sum(list,axis=1).tolist(), np.sum(list)]
+        }
+        return data_dict
+    else:
+      raise ValueError('List must contain nine numbers.')
